0223828b30e33cfa50d64ff007c766d744e334f0
Eduardo J. Guerra <edujguerra@gmail.com>
2022-11-20 16:15:38 -0300

    2211201615

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { Header } from "./components/Header";
 import Post from "./Post"
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post
-        author= "Eduardo Guerra"
-        content= "Conteudo do texto." />
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+
+        <main>
+          <Post
+            author= "Eduardo Guerra"
+            content= "Conteudo do texto." />
+
+          <Post 
+            author= "Eduardo Jacobsen"
+            content= "Novo Conteudo do texto." />
+        </main>
+
+      </div>
     </div>
   )
 }
